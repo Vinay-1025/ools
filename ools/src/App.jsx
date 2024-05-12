057a4387634bb121b55f667c71033ca4430c0f11
Vinay-1025 <darkeaglerules6ios@gmail.com>
2024-05-12 19:01:11 +0530

    Push the files

---
+++ ools/src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
+import About from './components/About/About'
+import Gallery from './components/Gallery/Gallery'
+import Testimonials from './components/Testimonials/Testimonials'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Videoplayer from './components/Videoplayer/Videoplayer'
+
+const App = () => {
+
+  const [playState,setPlayState] = useState(false);
+
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <div className="container">
+        <Title subTitle='About University' title='RGUKT - SKLM'/>
+        <About setPlayState = {setPlayState}/>
+        <Title subTitle='student space' title='What We Offer'/>
+        <Programs/>
+        <Title subTitle='gallery' title='Campus Memories'/>
+        <Gallery/>
+        <Title subTitle='Testimonials' title='Students View'/>
+        <Testimonials/>
+        <Title subTitle='contact us' title='Get In Touch'/>
+        <Contact/>
+        <Footer/>
+      </div>
+      <Videoplayer playState = {playState} setPlayState = {setPlayState}/>
+    </div>
+  )
+}
+
+export default App
